6966079e5a9c3b54cafe2be1c7d198281203f594
Anisa173 <celaanisa07@gmail.com>
2026-01-03 13:34:26 +0100

    jsdfgjgsd

--- src/app/Shared/services/actors.service.ts
+++ src/app/Shared/services/actors.service.ts
@@ -32,7 +32,7 @@ export class ActorsService {
   getActorFilmById(_actorId: number, _idFilm: number): Observable<Actors> {
     return this.httpC.get<Actors>('${this.apiUrl}/${idFilm}/${actorId}');
   }
-  getActorByFilmId(_actorId: number, _idFilm: number): Observable<Actors[]> {
-    return this.httpC.get<Actors[]>('${this.apiUrl}/retrieve/${idFilm}/{actorId}');
+  getActorByFilmId(_actorId: number, _idFilm: number): Observable<Actors> {
+    return this.httpC.get<Actors>('${this.apiUrl}/retrieve/${idFilm}/{actorId}');
   }
 }
